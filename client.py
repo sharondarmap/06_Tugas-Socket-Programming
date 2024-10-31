0c3af388065e987a457003ad69f809f002d9b79e
sharondarmap <19623106@std.stei.itb.ac.id>
2024-10-31 15:08:32 +0700

    feat: new TCP implement

--- client.py
+++ client.py
@@ -1,19 +1,50 @@
 import socket
 import threading
 
+
 def receive_messages(sock):
     """Menerima pesan dari server dan mencetaknya."""
     while True:
         try:
             data, _ = sock.recvfrom(1024)
-            print(data.decode())
+            message = data.decode()
+            
+            if message.isdigit():
+                ack_num = int(message)
+                print(f"Received ACK for sequence {ack_num}")
+                
+            else:
+                print(message)
         except:
             print("Maaf koneksi telah terputus ૮(˶ㅠ︿ㅠ)ა")
             break
 
+def send_with_ack(sock, message, server_addr, sequence_number):
+    """Kirim pesan dengan nomor urut dan tunggu ACK."""
+    attempts = 0
+    while attempts < 5:
+        try:
+            packet = f"{sequence_number}:{message}"
+            sock.sendto(packet.encode(), server_addr)
+
+            # Tunggu ACK
+            sock.settimeout(2)
+            ack, _ = sock.recvfrom(1024)
+            ack_num = int(ack.decode())
+            
+            # Jika ACK sesuai dengan nomor urut, kirim berhasil
+            if ack_num == sequence_number:
+                sock.settimeout(None)
+                return True
+        except socket.timeout:
+            attempts += 1
+            print(f"Timeout, mengirim ulang pesan (Percobaan {attempts}/5)")
+    return False
+
 # Konfigurasi koneksi
 server_ip = input("Masukkan IP Server : ")
 server_port = int(input("Masukkan Port Server : "))
+server_addr = (server_ip, server_port)
 
 # Inisialisasi socket UDP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -24,16 +55,17 @@ while True:
     password = input("Masukkan Password : ")
 
     # Kirim username dan password ke server
-    client_socket.sendto(f"{username}:{password}".encode(), (server_ip, server_port))
-
+    client_socket.sendto(f"{username}:{password}".encode(), server_addr)
     # Menerima respons dari server
     response, _ = client_socket.recvfrom(1024)
     response_msg = response.decode()
     print(response_msg)
 
     # Jika terhubung ke chatroom, keluar dari loop
-    if response_msg == "Selamat Anda telah terhubung ke chatroom!( ˶ˆᗜˆ˵ )":
+    if response_msg == "Selamat Anda telah terhubung ke chatroom.":
         break
+
+sequence_number = 1
 
 # Jalankan thread untuk menerima pesan
 thread = threading.Thread(target=receive_messages, args=(client_socket,))
@@ -44,7 +76,8 @@ thread.start()
 try:
     while True:
         message = input()
-        client_socket.sendto(message.encode(), (server_ip, server_port))
+        if send_with_ack(client_socket, message, server_addr, sequence_number):
+            sequence_number += 1  # Increment sequence number after successful send
 except KeyboardInterrupt:
     print("Keluar dari chat.")
 finally:
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 # Konfigurasi server
 IP = "0.0.0.0"  # Menerima koneksi dari semua interface
@@ -11,34 +12,74 @@ server_socket.bind((IP, PORT))
 
 print(f"Server berjalan di {IP}:{PORT}")
 
-clients = {}  # {alamat: username}
+clients = {}  # {alamat: [username, sequence_number]}
 
-def broadcast(message, source_addr):
-    """Meneruskan pesan ke semua client kecuali pengirim."""
+def send_with_ack(message, addr, sequence_number):
+    """Mengirim pesan dengan nomor urut dan menunggu ACK."""
+    attempts = 0
+    while attempts < 5:
+        try:
+            # Tambahkan nomor urut ke pesan
+            packet = f"{sequence_number}:{message}"
+            server_socket.sendto(packet.encode(), addr)
+
+            # Tunggu ACK dari client
+            server_socket.settimeout(2)
+            ack, _ = server_socket.recvfrom(1024)
+            ack_num = int(ack.decode())
+
+            # Jika ACK sesuai dengan nomor urut, kirim berhasil
+            if ack_num == sequence_number:
+                server_socket.settimeout(None)
+                return True
+        except (socket.timeout, ValueError):
+            attempts += 1
+            print(f"Timeout, mengirim ulang pesan ke {addr} (Percobaan {attempts}/5)")
+    
+    server_socket.settimeout(None)
+    return True
+
+def broadcast(message, source_addr, sequence_number):
+    """Meneruskan pesan ke semua client kecuali pengirim dengan menggunakan ACK."""
     for addr in clients:
         if addr != source_addr:
-            server_socket.sendto(message.encode(), addr)
+            send_with_ack(message, addr, sequence_number)
+            
+def handle_client_reg(data, addr):
+    username, password = data.split(":", 1)
+    
+    if password != PASSWORD:
+        server_socket.sendto("Password salah, silakan coba lagi.".encode(), addr)
+    elif username in [user[0] for user in clients.values()]:
+        server_socket.sendto("Username telah digunakan, silakan coba lagi.".encode(), addr)
+    else:
+        clients[addr] = [username, 1]
+        server_socket.sendto("Selamat Anda telah terhubung ke chatroom.".encode(), addr)
+        print(f"{username} bergabung dari {addr}")
 
 while True:
+    server_socket.settimeout(None)
     data, addr = server_socket.recvfrom(1024)
     msg = data.decode()
 
     if addr not in clients:
-        # Memproses pesan registrasi
-        username, password = msg.split(":", 1)
-        if (password != PASSWORD) and (username in clients.values()) :
-            server_socket.sendto("Username telah digunakan dan password salah, ayo coba lagi!!!(ง'̀-'́)ง".encode(), addr)
-        elif password != PASSWORD:
-            server_socket.sendto("Password salah, ayo coba lagi!!!(ง'̀-'́)ง".encode(), addr)
-        elif username in clients.values():
-            server_socket.sendto("Username telah digunakan, ayo coba lagi!!!(ง'̀-'́)ง".encode(), addr)
-            
-        else:
-            clients[addr] = username
-            server_socket.sendto("Selamat Anda telah terhubung ke chatroom!( ˶ˆᗜˆ˵ )".encode(), addr)
-            print(f"{username} bergabung dari {addr}")
+        handle_client_reg(msg, addr)
     else:
         # Menerima dan meneruskan pesan ke client lain
-        username = clients[addr]
-        print(f"[{username}] {msg}")
-        broadcast(f"[{username}] {msg}", addr)
+        username, sequence_number = clients[addr]
+        try:
+            packet_seq, msg_text = msg.split(":", 1)
+            packet_seq = int(packet_seq)
+        except ValueError:
+            print("Received malformed packet; discarding.")
+            continue
+        
+        if packet_seq == sequence_number:
+            print(f"[{username}] {msg_text}")
+            
+            # Kirim ACK ke pengirim
+            server_socket.sendto(str(packet_seq).encode(), addr)
+            clients[addr][1] += 1  # Increment the sequence number for the next message
+            broadcast(f"[{username}] {msg_text}", addr, sequence_number)
+        else:
+            print(f"Received out-of-order packet from {username} at {addr}, discarding.")
